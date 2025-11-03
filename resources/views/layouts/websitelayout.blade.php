6bd43078008053924f1840a027358b5980a3e83a
techwithbee <techspace00001@gmail.com>
2025-11-03 20:36:14 +0300

    fix

--- resources/views/layouts/websitelayout.blade.php
+++ resources/views/layouts/websitelayout.blade.php
@@ -4,6 +4,8 @@
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <link rel="icon" type="image/x-icon" href="favicon.ico" />
+
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <!-- Basic SEO Meta Tags -->
 <title>SR Temple Jewellery | Handcrafted Temple Jewellery by Artisans</title>
